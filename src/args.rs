8e23479303c2c7342055ee134fad83e2dea7b66b
Arno Geismar <arnogeismar@Arnos-MacBook-Pro.local>
2022-07-08 16:42:57 +0200

    bugfix changed command names

--- src/args.rs
+++ src/args.rs
@@ -1,13 +1,12 @@
-use clap:: {
-  Args,
-  Parser,
-};
+use clap::Parser;
 
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
 pub struct DoArgs {
-  // The command to execute!
-  pub command: String,
-  // The subcommand to execute!
-  pub message: String,
+    // The command to execute!
+    pub command: String,
+
+    // The message!
+    #[clap(short, long, default_value = "")]
+    pub message: String,
 }
--- src/git.rs
+++ src/git.rs
@@ -21,57 +21,59 @@ pub fn git_cli(args: DoArgs) {
             println!("Pushed to remote");
         }
         "pull" | "Pull" | "PULL" => {
-            let mut command = Command::new("git");
-            command.args(["pull"]);
-            command.output().unwrap();
+            let mut command4 = Command::new("git");
+            command4.args(["pull"]);
+            command4.output().unwrap();
             println!("Pulled from remote");
         }
         "status" | "Status" | "STATUS" => {
-            let mut command = Command::new("git");
-            command.args(["status"]);
-            command.output().unwrap();
-            println!("Status");
+            let mut command5 = Command::new("git");
+            command5.args(["status"]);
+            println!(
+                "{:?}",
+                format!("{:?}", String::from_utf8(command5.output().unwrap().stdout))
+            );
         }
         "log" | "Log" | "LOG" => {
-            let mut command = Command::new("git");
-            command.args(["log"]);
-            command.output().unwrap();
+            let mut command6 = Command::new("git");
+            command6.args(["log"]);
+            command6.output().unwrap();
             println!("Log");
         }
         "clone" | "Clone" | "CLONE" => {
-            let mut command = Command::new("git");
-            command.args(["clone", &args.message]);
-            command.output().unwrap();
+            let mut command7 = Command::new("git");
+            command7.args(["clone", &args.message]);
+            command7.output().unwrap();
             println!("Cloned");
         }
         "init" | "Init" | "INIT" => {
-            let mut command = Command::new("git");
-            command.args(["init"]);
-            command.output().unwrap();
+            let mut command8 = Command::new("git");
+            command8.args(["init"]);
+            command8.output().unwrap();
             println!("Initialized repo with GIT");
         }
         "add" | "Add" | "ADD" => {
-            let mut command = Command::new("git");
-            command.args(["add", "."]);
-            command.output().unwrap();
+            let mut command9 = Command::new("git");
+            command9.args(["add", "."]);
+            command9.output().unwrap();
             println!("Added everything");
         }
         "commit" | "Commit" | "COMMIT" => {
-            let mut command = Command::new("git");
-            command.args(["commit", "-m", &args.message]);
-            command.output().unwrap();
+            let mut command10 = Command::new("git");
+            command10.args(["commit", "-m", &args.message]);
+            command10.output().unwrap();
             println!("Committed with message: {}", args.message);
         }
         "push" | "Push" | "PUSH" => {
-            let mut command = Command::new("git");
-            command.args(["push"]);
-            command.output().unwrap();
+            let mut command11 = Command::new("git");
+            command11.args(["push"]);
+            command11.output().unwrap();
             println!("Pushed to remote");
         }
         "branch" | "Branch" | "BRANCH" => {
-            let mut command = Command::new("git");
-            command.args(["checkout", "-b", &args.message]);
-            command.output().unwrap();
+            let mut command12 = Command::new("git");
+            command12.args(["checkout", "-b", &args.message]);
+            command12.output().unwrap();
             println!("Checked out branch: {}", args.message);
         }
 
